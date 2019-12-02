b84c2f27f64772c31b4112ecb04e4e18f56d337f
Arthur L <arthurifccl@gmail.com>
2019-12-02 15:28:46 -0300

    Deploy fixes

--- src/main/java/com/mapeamento/Main.java
+++ src/main/java/com/mapeamento/Main.java
@@ -10,10 +10,20 @@ import static spark.Spark.*;
 
 
 public class Main {
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
     public static void main(String[] args) {
         Gson gson = new Gson();
         DB.init();
 
+        port(getHerokuAssignedPort());
 
         before((request, response) -> response.type("application/json"));
         internalServerError((req, res) -> gson.toJson(ResponseUtil.error("500")));
--- src/main/java/com/mapeamento/repository/DB.java
+++ src/main/java/com/mapeamento/repository/DB.java
@@ -4,8 +4,32 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
 
 public class DB {
+
+    private static class ConnectionData {
+        static String user;
+        static String pass;
+        static String url;
+    }
+
+    private static void loadHerokuVars() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("DB_PASS") != null
+                && processBuilder.environment().get("DB_USER") != null
+                && processBuilder.environment().get("DB_URL") != null) {
+            ConnectionData.url = processBuilder.environment().get("DB_URL");
+            ConnectionData.pass = processBuilder.environment().get("DB_PASS");
+            ConnectionData.user = processBuilder.environment().get("DB_USER");
+            return;
+        }
+        ConnectionData.url = "jdbc:mysql://localhost:3306/bd3";
+        ConnectionData.pass = "";
+        ConnectionData.user = "root";
+    }
 
     private static SessionFactory factory;
 
@@ -13,7 +37,16 @@ public class DB {
         if (factory != null) {
             return;
         }
-        factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        loadHerokuVars();
+        Configuration hConfig = new Configuration().configure("hibernate.cfg.xml");
+        Properties cfg = hConfig.getProperties();
+        cfg.put(Environment.USER, ConnectionData.user);
+        cfg.put(Environment.PASS, ConnectionData.pass);
+        cfg.put(Environment.URL, ConnectionData.url);
+
+        factory = hConfig.buildSessionFactory();
+
+
     }
 
     public static void init(){
